b62bdd796c089070015b3aba050a33fdd997dda5
ShraddhaIrale <96858107+ShraddhaIrale@users.noreply.github.com>
2022-02-14 19:27:46 +0530

    Add files via upload

---
+++ Handling User Authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,8 @@
+package com.project.Authentication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+public class AuthenticationApplicationTests {
+	@Test
+	public void contextLoads() {
+	}}
